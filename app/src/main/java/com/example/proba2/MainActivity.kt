dbebe45584750d18599b155534deff76062efa4e
User Mac <usermac@192.168.0.129>
2023-07-17 16:11:22 +0500

    Initial commit message

---
+++ app/src/main/java/com/example/proba2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.proba2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+//    hello word
+
+    }
+}
