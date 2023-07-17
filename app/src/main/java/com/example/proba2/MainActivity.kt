1646a9a5652217890efce952d290a58f786937b7
usermac <vaisovshohruh@gmail.com>
2023-07-17 16:15:49 +0500

    log commit

--- app/src/main/java/com/example/proba2/MainActivity.kt
+++ app/src/main/java/com/example/proba2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.proba2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,9 @@ class MainActivity : AppCompatActivity() {
 
 
 //    hello word
+
+        val a = 5
+        Log.d("TTT", "onCreate:  proba2")
 
     }
 }
